12d3c6d4be442e401ea75846378260d5ddac2beb
Ubuntu <ubuntu@ip-172-31-37-15.ec2.internal>
2019-07-16 18:18:25 +0000

    Fetched 3 popluar records,modified css and index pages

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
 from flask import Flask,render_template
+from flask_pymongo import PyMongo,DESCENDING
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"]='myCookingDB'
+app.config["MONGO_URI"]=os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    top_three_viewed_recipes=mongo.db.recipes.find().sort([('views',DESCENDING)]).limit(3)
+    return render_template("index.html",recipes=top_three_viewed_recipes)
     
 if __name__ == '__main__':
     app.run(host = os.environ.get('IP'),
