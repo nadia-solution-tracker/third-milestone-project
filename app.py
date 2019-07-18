d29b777a3122e06818b03d0a6befcf2c2e9623ca
Ubuntu <ubuntu@ip-172-31-37-15.ec2.internal>
2019-07-18 17:39:46 +0000

    User recipes html page,user_recipe code ,css styles and pagination

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask,render_template,request,session,flash,redirect,url_for
 from flask_pymongo import PyMongo,DESCENDING
 from bson.objectid import ObjectId 
+from flask_paginate import Pagination, get_page_args
 import bcrypt
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ app.config["MONGO_URI"]=os.getenv('MONGO_URI')
 app.secret_key="some_secret"
 
 mongo = PyMongo(app)
+ITEMS_PER_PAGE = 6
 
 @app.route('/')
 def index():
@@ -78,7 +80,21 @@ def insert_recipe():
     data['upvotes']= 0
     data['user_name']=session['user_name']
     recipes.insert_one(data)
-    return redirect(url_for('login'))
+    return redirect(url_for('user_recipes'))
+    
+@app.route('/user_recipes')
+def user_recipes():
+    if not 'user_name' in session:
+        return redirect('/login')
+    else:
+        #https://gist.github.com/Faizanq/a4d07f1a9624bdf6d3ddd9d4d6cc1b2d
+        page, per_page, offset = get_page_args(page_parameter='page',per_page_parameter='per_page')
+        total = mongo.db.recipes.count_documents({})
+        my_recipes_fetched = mongo.db.recipes.find().sort([('upvotes', DESCENDING)]).skip((page - 1)*ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+        pagination = Pagination(page=page,per_page=ITEMS_PER_PAGE,total=total,record_name='Recipes')
+        return render_template('userrecipes.html',recipes=my_recipes_fetched,username=session['user_name'],page=page,
+                           per_page=per_page,
+                           pagination=pagination)
     
 if __name__ == '__main__':
     app.run(host = os.environ.get('IP'),
